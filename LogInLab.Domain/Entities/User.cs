57c6a387569f223ddcb6a138eca25c2c7c239ff8
GuillermoSaSu <guillermosasu99@gmail.com>
2026-07-29 12:48:15 +0200

    Add User entity and database configuration
    The "User" class has been created in "LogInLab.Domain.Entities" with relevant properties.
    References to Entity Framework Core and Npgsql have been added to the infrastructure project.
    The initial migration for the "users" table has been generated, and the database context has been updated to include this entity.
    Additionally, the connection string has been configured in "appsettings.json".
    
    This is a test for the connection with the database container.

---
+++ LogInLab.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace LogInLab.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool EmailVerified { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int FailedLoginAttempts { get; set; }    
+    }
+}
---
+++ LogInLab.Infrastructure/Persistence/LogInLabDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LogInLab.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogInLab.Infrastructure.Persistence
+{
+    public class LogInLabDbContext : DbContext
+    {
+        public LogInLabDbContext(DbContextOptions<LogInLabDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LogInLabDbContext).Assembly);
+        }
+    }
+}
